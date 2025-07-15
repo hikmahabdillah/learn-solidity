25c6c2b2da6be11c8a5cb55ca84a65a19aa4c56c
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-07-15 16:31:47 +0700

    fixed array

---
+++ contracts/ArrayContract.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0; // versi 0.6.0 ke atas
+
+contract ArrayContract{
+    // Array
+    uint[5] arrayData; // state variable public n permanent
+
+    // fixed array, bisa digunakan untuk merubaha nilai state
+    function getDataArray() public returns(uint){
+        arrayData = [uint(10),20,30,40,50]; // mendefinisikan tipe data di depan saja
+
+        return (arrayData[0]);
+    }
+}
