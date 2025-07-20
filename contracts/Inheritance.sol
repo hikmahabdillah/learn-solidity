74f4b216d0c16c0740e9195a78ce06bf1f5b2471
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-07-20 19:13:26 +0700

    single inheritance

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -4,13 +4,22 @@ pragma solidity >=0.6.0 <0.9.0; // versi 0.6.0 ke atas
 contract Parent{
     uint internal id; // bisa diakses oleh kontrak turunannya
 
-    function setValue(uint _value)public {
+    function setValue(uint _value)external {
         id = _value;
     }
 }
 
 contract FirstChild is Parent{
-    function getValue() public view returns(uint){
+    function getValue() external view returns(uint){
         return id;
+    }
+}
+
+contract Caller{
+    FirstChild fiChild = new FirstChild();
+
+    function inherit(uint _input)public returns(uint){
+        fiChild.setValue(_input);
+        return fiChild.getValue();
     }
 }
